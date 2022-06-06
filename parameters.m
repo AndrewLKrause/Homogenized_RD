212aa8e386d4b102bad7e61c9b3db0f2f844688f
AndrewLKrause <42071282+AndrewLKrause@users.noreply.github.com>
2022-06-06 12:41:50 +0100

    Update parameters.m

--- parameters.m
+++ parameters.m
@@ -1,7 +1,7 @@
 % Geometric Parameters
 theta = 0.5; % Ratio of patch lengths
-Pn = 2; % Number of patches of each kind (so 2*Pn total patches)
-eta = 1e9; % Coupling constant between patches
+Pn = 1; % Number of patches of each kind (so 2*Pn total patches)
+eta = Inf; % Coupling constant between patches
 L = 100; % Length of macroscale domain
 pL = L/(2*Pn); % Length of individual patch
 
@@ -16,27 +16,23 @@ tspan = linspace(0,T,1e3); % Interpolate solution on [0,T] with 1e2 points
 x = linspace(0,L,N);
 
 % Kinetic/ratio of diffusion parameters
-a1 = 0.01;
-a2 = 0.1;
-b1 = 1.2;
-b2 = 9;
-D1u = 1;
-D2u = 1;
-D1v = 1500;
-D2v = 1500;
+%a1 = 0.01;b1 = 1.2;a2 = 0.1;b2 = 9;
+a1 = 0.01;b1 = 1.2;a2 = 0.4;b2 = 5.5;
+D1u = 1;D1v = 1000;D2u = 1;D2v = 1000;
+
+
+
 
 % Kinetics
-f1 = @(u,v)a1-u+u.^2.*v;
-f2 = @(u,v)a2-u+u.^2.*v;
-g1 = @(u,v)b1-u.^2.*v;
-g2 = @(u,v)b2-u.^2.*v;
+f1 = @(u,v)a1-u+u.^2.*v;g1 = @(u,v)b1-u.^2.*v;
+%f2 = @(u,v)a2-u+u.^2.*v;g2 = @(u,v)b2-u.^2.*v;
+%f1 = @(u,v).01+u.^2./v-a2*u;g1 = @(u,v)u.^2-b2*v;
+f2 = @(u,v).01+u.^2./v-a2*u;g2 = @(u,v)u.^2-b2*v;
 
 % Steady states
-u1ss = a1+b1;
-v1ss = b1/(a1+b1)^2;
-u2ss = a2+b2;
-v2ss = b2/(a2+b2)^2;
-
+u1ss = a1+b1;v1ss = b1/(a1+b1)^2;
+%u2ss = a2+b2;v2ss = b2/(a2+b2)^2;
+u2ss = (.01+b2)/a2;v2ss = u2ss^2/b2;
 % Create a normally distributed vector of size N, mean 1, stdev 1e-2.
 rng(1);
 rand_vec = 1+1e-2*randn(2*N,1); 
